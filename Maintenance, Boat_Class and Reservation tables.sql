34490656b954fb402fb9e59fb8650ea50e813a45
BAKAJ77 <44305796+BAKAJ77@users.noreply.github.com>
2022-02-18 06:12:04 +0000

    Made constraint changes according to specification

--- Maintenance, Boat_Class and Reservation tables.sql
+++ Maintenance, Boat_Class and Reservation tables.sql
@@ -1,10 +1,9 @@
 create table Boat_Class (
 	ClassID int primary key auto_increment,
-    Classification varchar(20),
-    Size varchar(15),
+    Classification varchar(20) CHECK (Classification IN ("very large", "large", "standard", "small")),
+    Size varchar(15) CHECK (Classification IN ("very large", "large", "standard", "small")),
     HalfDayPrice decimal (5,2),
     FullDayPrice decimal (5,2)
-    
 );
 
 create table Reservation (
@@ -14,22 +13,18 @@ create table Reservation (
     ReserveDate date,
     PeriodOfHire varchar(10),
     
-
-	constraint fk_ClassID foreign key (ClassID)
-		references Boat_Class (ClassID)
-    
-    
-    
+	constraint fk_ClassID foreign key (ClassID) references Boat_Class (ClassID) ON UPDATE CASCADE
 );
 
 create table Maintenance (
 	BoatID int,
-    MainRefNo int,
+    MainRefNo int AUTO_INCREMENT,
     FaultDetails varchar(100),
     FaultDate date,
     ActionDetails varchar(50),
     ActionDate date,
+    Priority INT DEFAULT 2,
     
-	primary key (BoatID, MainRefNo)
-    
+	constraint primary key (BoatID, MainRefNo),
+    CONSTRAINT FK_BOAT_ID FOREIGN KEY (BoatID) REFERENCES Boat (BoatID) ON UPDATE CASCADE
 );
