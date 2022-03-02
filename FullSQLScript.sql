3e279921f5651ad1c01bb454d0c50467d2586301
BAKAJ77 <44305796+BAKAJ77@users.noreply.github.com>
2022-03-02 15:16:45 +0000

    Update FullSQLScript.sql

--- FullSQLScript.sql
+++ FullSQLScript.sql
@@ -4,18 +4,18 @@
 DROP TABLE IF EXISTS Disposal;
 DROP TABLE IF EXISTS Dealer;
 DROP TABLE IF EXISTS Rental;
-DROP TABLE IF EXISTS Customer;
 
+DROP TABLE IF EXISTS Customer;
 DROP TABLE IF EXISTS Reservation;
 DROP TABLE IF EXISTS Maintenance;
-DROP TABLE IF EXISTS Boat_Class;
 
-DROP TABLE IF EXISTS Boat;
 DROP TABLE IF EXISTS Spare_Part;
+DROP TABLE IF EXISTS Boat;
+DROP TABLE IF EXISTS Boat_Class;
 DROP TABLE IF EXISTS Supplier;
 
 /************************************************************************************************************************/
-/* Create Supplier, Boat and Spare Part tables (By Benjamin) */
+/* Create Tables */
 
 CREATE TABLE Supplier (
 	SupplierID INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,6 +23,14 @@ CREATE TABLE Supplier (
     PostCode VARCHAR(8),
     Telephone VARCHAR(15),
     Email VARCHAR(20) UNIQUE
+);
+
+create table Boat_Class (
+	ClassID int primary key auto_increment,
+    Classification varchar(20) CHECK (Classification IN ("Motor boat", "Row boat", "Sail boat")),
+    Size varchar(15) CHECK (Size IN ("very large", "large", "standard", "small")),
+    HalfDayPrice decimal (5,2),
+    FullDayPrice decimal (5,2)
 );
 
 CREATE TABLE Boat (
@@ -34,6 +42,7 @@ CREATE TABLE Boat (
     Model VARCHAR(20),
     
     CONSTRAINT FK_BOAT_SUPPLIER_ID FOREIGN KEY (SupplierID) REFERENCES Supplier (SupplierID) ON UPDATE CASCADE,
+    CONSTRAINT FK_BOAT_CLASS_ID FOREIGN KEY (ClassID) REFERENCES Boat_Class (ClassID) ON UPDATE CASCADE,
     CONSTRAINT PURCHASE_VALUE_CHK CHECK (PurchaseValue >= 0)
 );
 
@@ -45,17 +54,6 @@ CREATE TABLE Spare_Part (
     Ordered BIT,
     
     CONSTRAINT FK_SPARE_PART_SUPPLIER_ID FOREIGN KEY (SupplierID) REFERENCES Supplier (SupplierID) ON UPDATE CASCADE
-);
-
-/************************************************************************************************************************/
-/* Create Boat Class, Reservation and Maintainance tables (By Umair) */
-
-create table Boat_Class (
-	ClassID int primary key auto_increment,
-    Classification varchar(20) CHECK (Classification IN ("very large", "large", "standard", "small")),
-    Size varchar(15) CHECK (Classification IN ("very large", "large", "standard", "small")),
-    HalfDayPrice decimal (5,2),
-    FullDayPrice decimal (5,2)
 );
 
 create table Reservation (
@@ -81,9 +79,6 @@ create table Maintenance (
     CONSTRAINT FK_BOAT_ID FOREIGN KEY (BoatID) REFERENCES Boat (BoatID) ON UPDATE CASCADE
 );
 
-/************************************************************************************************************************/
-/* Create Customer and Rental tables (By Yves) */
-
 CREATE TABLE Customer (
 CustomerID  INT PRIMARY KEY,
 First_name  VARCHAR(45),
@@ -93,7 +88,6 @@ Email VARCHAR(45),
 Phone_number INT,
 Postcode VARCHAR(45)
 );
-
 
 CREATE TABLE Rental (
 RentalID INT PRIMARY KEY,
@@ -107,22 +101,17 @@ CONSTRAINT customer_renting FOREIGN KEY (customerID) REFERENCES Customer(custome
 CONSTRAINT boat_key FOREIGN KEY(BoatID) REFERENCES Boat (BoatID)
 );
 
-/************************************************************************************************************************/
-/* Create Dealer and Disposal tables (By Aaron) */
-
 create table Dealer (
 DealerID int primary key auto_increment,
 DealerName varchar(50),
 Address varchar(100),
 TelephoneNumber int,
 Email varchar(100)
-
-
 );
 
 create table Disposal (
-DealerID int ,
-BoatNumber int ,
+DealerID int,
+BoatNumber int,
 SaleDate date,
 SalePrice int,
 
@@ -132,3 +121,44 @@ constraint fk_DealerID foreign key (DealerID) references Dealer (DealerID) ON UP
 
 /************************************************************************************************************************/
 
+Insert into Boat_Class
+	(ClassID, Classification, Size, HalfDayPrice, FullDayPrice)
+Values	(1, 'Sail boat', 'very large', 240, 170),
+		(2, 'Sail boat', 'large', 180, 120),
+		(3, 'Sail boat', 'standard', 160, 100),
+		(4, 'Sail boat', 'small', 140, 90),
+		(5, 'Motor boat', 'very large', 280, 175),
+		(6, 'Motor boat', 'large', 240, 150),
+		(7, 'Motor boat', 'standard', 200, 125 ),
+		(8, 'Motor boat', 'small', 170, 110),
+		(9, 'Row boat', 'very large', 160, 100),
+		(10, 'Row boat', 'large', 140, 90),
+		(11, 'Row boat', 'standard', 120, 80 ),
+		(12, 'Row boat', 'small', 110, 70);
+
+INSERT INTO Boat (BoatID, SupplierID, ClassID, PurchaseDate, PurchaseValue, Model) VALUES
+	(1, 1, 7, "2010-11-22", 3910, "Explorer"),
+    (2, 2, 1, "2009-10-12", 3840, "TurboSail"),
+    (3, 2, 2, "2015-10-12", 3240, "MasterSail"),
+    (4, 3, 4, "2015-11-12", 2040, "SmallSailor"),
+    (5, 3, 4, "2015-11-12", 2040, "SmallSailor"),
+    (6, 5, 6, "2015-01-14", 5440, "Grande"),
+    (7, 5, 6, "2015-01-14", 5440, "Grande"),
+    (8, 2, 7, "2015-01-14", 5440, "Turbo Mid"),
+    (9, 3, 10, "2015-01-12", 5440, "RowStream"),
+    (10, 4, 11, "2015-01-12", 5440, "RowerX"),
+    (11, 1, 2, "2016-01-10", 5440, "Explorer"),
+    (12, 1, 3, "2016-01-10", 5440, "Navigator"),
+    (13, 2, 7, "2018-02-14", 5440, "Turbo Mid"),
+    (14, 2, 7, "2018-02-14", 5440, "Turbo Mid"),
+    (15, 7, 6, "2018-01-14", 5440, "MasterBlaster"),
+    (16, 8, 10, "2018-01-10", 5440, "HappyRower"),
+    (17, 8, 11, "2018-01-10", 5440, "HappyRower"),
+    (18, 3, 9, "2017-01-09", 5440, "Streamer"),
+    (19, 4, 9, "2019-01-19", 5440, "Great Row"),
+    (20, 7, 5, "2018-01-14", 5440, "SuperBlaster"),
+    (21, 6, 11, "2017-01-09", 5440, "Lizard"),
+    (22, 5, 6, "2018-02-14", 5440, "Grande"),
+    (23, 2, 2, "2015-10-12", 5440, "MasterSail");
+
+/************************************************************************************************************************/
