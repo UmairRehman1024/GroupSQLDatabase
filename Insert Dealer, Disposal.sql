353ce786bd0ea863c41dde83c806bb6fe9ad0f82
AO0702 <99814787+AO0702@users.noreply.github.com>
2022-03-03 13:17:03 +0000

    Update Insert Dealer, Disposal.sql

--- Insert Dealer, Disposal.sql
+++ Insert Dealer, Disposal.sql
@@ -1,20 +1,26 @@
 INSERT INTO Dealer (DealerID, DealerName, TelephoneNumber, Address, Email)  
 VALUES (1, 'Dalis Vannoort', 07574137463, '77 A828, Appin, AP7 6GU', 'dvannoort0@salon.com'),
 (2, "Joe's Junk", 07365534221, '15 Back Lane, Buxton, BX7 5FY', 'JoesJunk@zdnet.com'),
-(3, 'Hoebart Kubera', 07874051869, '4 Finedon House, Marine 
-Parade, Littlestone, LS4 6GU', 'hkubera2@who.int'),
-(4, 'Eva Iacomettii', 07880072148, '9 Hartlands, Onslow Road, 
-Newent, NW5 8TU', 'eiacomettii3@admin.ch'),
+(3, 'Hoebart Kubera', 07874051869, '4 Finedon House, Marine Parade, Littlestone, LS4 6GU', 'hkubera2@who.int'),
+(4, 'Eva Iacomettii', 07880072148, '9 Hartlands, Onslow Road, Newent, NW5 8TU', 'eiacomettii3@admin.ch'),
 (5, 'Alley Pate', 07822040557, '07610 Arizona Alley, A67 8GU', 'apate4@gnu.org'),
-(6, 'Korrie Legge', 07380018233, '1076 Evesham Road, Astwood 
-Bank, DT5 8JO', 'klegge5@reference.com'),
+(6, 'Korrie Legge', 07380018233, '1076 Evesham Road, Astwood Bank, DT5 8JO', 'klegge5@reference.com'),
 (7, 'Minne Hinkens', 07978390430, '53 Balby Road, Balby, B7 8HK', 'mhinkens6@smh.com.au'),
 (8, 'Inigo MacAllaster', 07893419552, '1910 Farwell Plaza, G56 9FT', 'imacallaster7@blogspot.com'),
-(9, 'Linell Skeeles', 07532931207, '57 Great Russell Street, 
-London, NW1 8TU', 'lskeeles8@goo.gl'),
-(10, 'Sioux Drogan', 07417098738, '75 Thomas Parsons Square, 
-Ely, EL6 9GU', 'sdrogan9@dropbox.com')
+(9, 'Linell Skeeles', 07532931207, '57 Great Russell Street, London, NW1 8TU', 'lskeeles8@goo.gl'),
+(10, 'Sioux Drogan', 07417098738, '75 Thomas Parsons Square, Ely, EL6 9GU', 'sdrogan9@dropbox.com'),
+(11, 'Yves Neyo', 07694201011, '21 notverysmart road, m12 42k', 'yvesneyo@gmail.com' ),
+(12, 'Ben smalls', 07767902849, '47 rubicon lane, sk4 8gl', 'bensmalls@gmail.com'),
+(13, 'Umair johnson', 08001807664, '360 cygnet road, m30 12j', 'Drumairjohnson@.com'),
+(14, 'Elod sipos', 09020394069, '250 grand drive, m40 62p', 'Elod@gmail.com'),
+(15, 'Sherry oba', 07980102939, '187 oakland grove, sk8 l87', 'sherryoba@gmail.ocm')
 ;
 
 INSERT INTO Disposal (DealerID, BoatNumber, SaleDate, SalePrice)
-VALUES (2, 1, '2020-11-30', 1500.00);
+VALUES (2, 1, '2020-11-30', 1500.00 ),
+(11, 2, '2022-03-03', 400.00 ),
+(9, 3, '2019-06-19', 1000.00 ),
+(15, 4, '2022-08-20', 3075.00),
+(3, 5, '2022-10-24', 1750.00),
+(1, 6, '2018-05-15', 2200.00)
+;
